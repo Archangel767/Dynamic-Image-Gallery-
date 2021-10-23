36d2824676cac13c2673854c9154260390f0e960
Archangel767 <mjolnir365@gmail.com>
2021-10-23 17:40:35 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Course Title: CPNT 262 Assignment 2 - Dynamic Image Gallery
+- Author: James Atkinson
+
+## Github Links
+- Repo: https://github.com/Archangel767/cpnt262-a2
+- Pages: https://archangel767.github.io/cpnt262-a2/
+
+## Code Review
+- A small bug I ran into was my inability to reduce the gap size between the grid tiles no matter what value I tried to adjust.
+- For some reason, I had put the figure elements into an unordered list, which created unmodifiable "intendations" in the grid. I removed the list elements and adjusted the styling accordingly.
+- Everything started to work more predictably, and I learned that I need to stop jacking random code I find on the internet.  
+
+## Attributions
+- [Code] From JS tutorial (https://gist.github.com/acidtone/a5f40cda47c6d5c1a8e3815833572024) by Tony Grimes (https://gist.github.com/acidtone)
+- [Photo] (https://www.pexels.com/photo/body-of-water-with-rock-formation-2308893) by Matt Hardy (https://www.pexels.com/@matthardy) under [Pexels License] (http://www.pexels.com/license)]
+- [Photo] (https://www.pexels.com/photo/green-trees-near-rock-formation-2754200/) by Todd Trapani (https://www.pexels.com/@todd-trapani-488382) under [Pexels License] (http://www.pexels.com/license)]
+- [Photo] (https://www.pexels.com/photo/photo-of-forest-during-daytime-3222686/) by Tim Mossholder (https://www.pexels.com/@timmossholder) under [Pexels License] (http://www.pexels.com/license)]
+- [Photo] (https://www.pexels.com/es-es/foto/olas-del-mar-tempestuoso-y-playa-de-arena-3876447/) by Tomas Anunziata (https://www.pexels.com/es-es/@tomas-anunziata-129267) under [Pexels License] (http://www.pexels.com/license)]
+- [Photo] (https://www.pexels.com/es-es/foto/arboles-verdes-en-la-montana-4017166/) by Jenny Uhling (https://www.pexels.com/es-es/@jenny-uhling-2262740) under [Pexels License] (http://www.pexels.com/license)]
+- [Photo] (https://www.pexels.com/photo/calm-sea-and-mountains-at-sunset-time-4993160/) by Rachel Claire (https://www.pexels.com/@rachel-claire) under [Pexels License] (http://www.pexels.com/license)]
+- [Photo] (https://www.pexels.com/es-es/foto/persona-de-pie-sobre-la-formacion-rocosa-6150551/) by Matthew DeVries (https://www.pexels.com/es-es/@mattdvphotography) under [Pexels License] (http://www.pexels.com/license)]
+- [Photo] (https://www.pexels.com/de-de/foto/meer-natur-himmel-sonnenuntergang-6868659/) by Kira Schwarz (https://www.pexels.com/de-de/@kira-schwarz) under [Pexels License] (http://www.pexels.com/license)]
+- [Photo] (https://www.pexels.com/photo/brown-rocky-mountain-near-river-9544524/) by Arkhod (https://www.pexels.com/@arkhod-85162625) under [Pexels License] (http://www.pexels.com/license)]
+- Logo and Social Media icons from Font Awesome and modified in Adobe Photoshop - https://fontawesome.com
+
+
+
+ 
---
+++ assets/js/dynamic-gallery.js
@@ -0,0 +1,124 @@
+
+// Object literal array:
+const scenery = [
+  {
+    id: '01',
+    title: 'Coast',
+    description: 'Bunch of coast',
+    width: '1688px',
+    height: '2250px',
+    pathURL: 'assets/images/01.jpeg',
+    linkURL: 'https://www.pexels.com/photo/body-of-water-with-rock-formation-2308893',
+    credit:  'Matt Hardy',
+    creditURL: 'https://www.pexels.com/@matthardy'
+  },
+  {
+    id: '02',
+    title: 'Mesa',
+    description: 'A mesa',
+    width: '1501px',
+    height: '2250px',
+    pathURL: 'assets/images/02.jpeg',
+    linkURL: 'https://www.pexels.com/photo/green-trees-near-rock-formation-2754200/',
+    credit: 'Todd Trapani',
+    creditURL: 'https://www.pexels.com/@todd-trapani-488382',
+  },
+  {
+    id: '03',
+    title: 'Forest',
+    description: 'Bunch of trees',
+    width: '1500px',
+    height: '2250px',
+    pathURL: 'assets/images/03.jpeg',
+    linkURL:  'https://www.pexels.com/photo/photo-of-forest-during-daytime-3222686/',
+    credit: 'Tim Mossholder',
+    creditURL: 'https://www.pexels.com/@timmossholder',
+  },
+  {
+    id: '04',
+    title: 'Beach',
+    description: 'A beach',
+    width: '1264px',
+    height: '2250px',
+    pathURL: 'assets/images/04.jpeg',
+    linkURL:  'https://www.pexels.com/es-es/foto/olas-del-mar-tempestuoso-y-playa-de-arena-3876447/',
+    credit: 'Tomas Anunziata',
+    creditURL: 'https://www.pexels.com/es-es/@tomas-anunziata-129267',
+  },
+  {
+    id: '05',
+    title: 'Mountain',
+    description: 'Misty mountain',
+    width: '1494px',
+    height: '2250px',
+    pathURL: 'assets/images/05.jpeg',
+    linkURL:  'https://www.pexels.com/es-es/foto/arboles-verdes-en-la-montana-4017166/',
+    credit: 'Jenny Uhling',
+    creditURL: 'https://www.pexels.com/es-es/@jenny-uhling-2262740',
+  },
+  {
+    id: '06',
+    title: 'Sunset',
+    description: 'Bunch of sunset',
+    width: '1522px',
+    height: '2250px',
+    pathURL: 'assets/images/06.jpeg',
+    linkURL:  'https://www.pexels.com/photo/calm-sea-and-mountains-at-sunset-time-4993160/',
+    credit: 'Rachel Claire',
+    creditURL: 'https://www.pexels.com/@rachel-claire',
+  },
+  {
+    id: '07',
+    title: 'Canyon',
+    description: 'Bunch of canyon',
+    width: '1497px',
+    height: '2250px',
+    pathURL: 'assets/images/07.jpeg',
+    linkURL:  'https://www.pexels.com/es-es/foto/persona-de-pie-sobre-la-formacion-rocosa-6150551/',
+    credit: 'Matthew DeVries',
+    creditURL: 'https://www.pexels.com/es-es/@mattdvphotography',
+  },
+  {
+    id: '08',
+    title: 'Ocean',
+    description: 'Bunch of ocean',
+    width: '1688px',
+    height: '2250px',
+    pathURL: 'assets/images/08.jpeg',
+    linkURL: 'https://www.pexels.com/de-de/foto/meer-natur-himmel-sonnenuntergang-6868659/',
+    credit: 'Kira Schwarz',
+    creditURL: 'https://www.pexels.com/de-de/@kira-schwarz',
+  },
+  {
+    id: '09',
+    title: 'Rock',
+    description: 'A big rock',
+    width: '1500px',
+    height: '2250px',
+    pathURL: 'assets/images/09.jpeg',
+    linkURL:  'https://www.pexels.com/photo/brown-rocky-mountain-near-river-9544524/',
+    credit: 'Arkhod',
+    creditURL: 'https://www.pexels.com/@arkhod-85162625',
+  },
+
+]
+
+let output = '';
+
+// forEach loop:
+scenery.forEach(function (nature) {
+  output += `
+          <figure>
+            <a href="${nature.linkURL}">
+              <img src="${nature.pathURL}" alt="${nature.title}">
+              <figcaption>${nature.title}</figcaption>
+            </a>
+          </figure>  
+            `;
+});
+
+//  HTML output:
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML = output;
+
+document.querySelector('.gallery').innerHTML = output;
